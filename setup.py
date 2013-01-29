b4a3208c348d1669b21d383f13251bf840f95670
ignaciocontreras <ignacio@nachovia.com>
2013-01-29 12:12:32 +0100

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='culebra',
+      version='0.0.1',
+      description='Simple file-based persistent queue',
+      author='Ignacio Contreras Pinilla',
+      url='http://nachovia.com',
+      packages=['culebra'],
+      )
